edbb53bdbfbbb02dafeb6896423f5931afea1f25
Jeff R <langboost@gmail.com>
2015-10-12 15:39:55 +0530

    Version 1.0.4. Fixes #3.

--- deploy/Build.bat
+++ deploy/Build.bat
@@ -21,24 +21,24 @@ REM ======================= build =======================================
 
 REM Build out staging NuGet folder structure
 mkdir %StagingPath%
-mkdir %StagingPath%\build\uap10.0
+mkdir %StagingPath%\build\native
 mkdir %StagingPath%\lib\uap10.0
 mkdir %StagingPath%\include
-mkdir %StagingPath%\runtimes\win10-arm\lib\uap10.0
-mkdir %StagingPath%\runtimes\win10-x64\lib\uap10.0
-mkdir %StagingPath%\runtimes\win10-x86\lib\uap10.0
+mkdir %StagingPath%\runtimes\win10-arm\native
+mkdir %StagingPath%\runtimes\win10-x64\native
+mkdir %StagingPath%\runtimes\win10-x86\native
 
 REM Stage platform-independent files
-copy deploy\nuget\curve25519-uwp.targets %StagingPath%\build\uap10.0
+copy deploy\nuget\curve25519-uwp.targets %StagingPath%\build\native
 copy curve25519\Curve25519_Internal.h %StagingPath%\include
 copy curve25519\Curve25519Native.h %StagingPath%\include
 
 REM build x86
 %MSBuildPath% curve25519\curve25519.vcxproj /property:Configuration=Release /property:Platform=x86
 REM stage x86
-copy curve25519\Release\curve25519\curve25519.dll %StagingPath%\runtimes\win10-x86\lib\uap10.0
-copy curve25519\Release\curve25519\curve25519.winmd %StagingPath%\runtimes\win10-x86\lib\uap10.0
-copy curve25519\Release\curve25519\curve25519.pri %StagingPath%\runtimes\win10-x86\lib\uap10.0
+copy curve25519\Release\curve25519\curve25519.dll %StagingPath%\runtimes\win10-x86\native
+REM copy curve25519\Release\curve25519\curve25519.winmd %StagingPath%\runtimes\win10-x86\native
+copy curve25519\Release\curve25519\curve25519.pri %StagingPath%\runtimes\win10-x86\native
 
 REM copy winmd and dll to lib folder to serve as architecture-independent "ref assembly"
 copy curve25519\Release\curve25519\curve25519.winmd %StagingPath%\lib\uap10.0
@@ -46,19 +46,19 @@ copy curve25519\Release\curve25519\curve25519.winmd %StagingPath%\lib\uap10.0
 REM build x64
 %MSBuildPath% curve25519\curve25519.vcxproj /property:Configuration=Release /property:Platform=x64
 REM stage x64
-copy curve25519\x64\Release\curve25519\curve25519.dll %StagingPath%\runtimes\win10-x64\lib\uap10.0
-copy curve25519\x64\Release\curve25519\curve25519.winmd %StagingPath%\runtimes\win10-x64\lib\uap10.0
-copy curve25519\x64\Release\curve25519\curve25519.pri %StagingPath%\runtimes\win10-x64\lib\uap10.0
+copy curve25519\x64\Release\curve25519\curve25519.dll %StagingPath%\runtimes\win10-x64\native
+REM copy curve25519\x64\Release\curve25519\curve25519.winmd %StagingPath%\runtimes\win10-x64\native
+copy curve25519\x64\Release\curve25519\curve25519.pri %StagingPath%\runtimes\win10-x64\native
 
 REM build ARM
 %MSBuildPath% curve25519\curve25519.vcxproj /property:Configuration=Release /property:Platform=ARM
 REM stage ARM
-copy curve25519\ARM\Release\curve25519\curve25519.dll %StagingPath%\runtimes\win10-arm\lib\uap10.0
-copy curve25519\ARM\Release\curve25519\curve25519.winmd %StagingPath%\runtimes\win10-arm\lib\uap10.0
-copy curve25519\ARM\Release\curve25519\curve25519.pri %StagingPath%\runtimes\win10-arm\lib\uap10.0
+copy curve25519\ARM\Release\curve25519\curve25519.dll %StagingPath%\runtimes\win10-arm\native
+REM copy curve25519\ARM\Release\curve25519\curve25519.winmd %StagingPath%\runtimes\win10-arm\native
+copy curve25519\ARM\Release\curve25519\curve25519.pri %StagingPath%\runtimes\win10-arm\native
 
 REM create NuGet package
-REM (Note: Expect 6 Assembly outside lib folder warnings, due to WinRT custom packaging technique)
+REM (Note: Expect 3 Assembly outside lib folder warnings, due to WinRT arch specific dlls)
 pushd deploy\staging
 %NuGetPath% pack ..\nuget\curve25519-uwp.nuspec -outputdirectory ..
 popd
